0183caf577883277277dbf8c9002cd21ce42857e
Andrew Nichols <andrew@frenata.net>
2017-10-25 12:24:59 -0400

    Parse Jumps and Registers directly with 'Read' instances and readMaybe.
    Start using Either types to handle parsing errors.

--- Assembler.hs
+++ Assembler.hs
@@ -1,15 +1,18 @@
 module Assembler where
 
+import           Data.Maybe
+import           Text.Read
+
 data Instruction
-  = A Int
-  | C Operation
+  = AInstruction Int
+  | CInstruction Operation
   deriving (Show)
 
 data Register
-  = MReg
-  | DReg
-  | AReg
-  deriving (Show)
+  = M
+  | D
+  | A
+  deriving (Show, Read)
 
 data Jump
   = JGT
@@ -31,45 +34,41 @@ data Operation = Operation
   , jump :: Maybe Jump
   } deriving (Show)
 
+type Error = String
+
 assemble :: String -> Instruction
 assemble = undefined
 
-parse :: String -> Instruction
 parse inst@(x:xs) =
-  if x == '@'
-    then A (read xs :: Int)
-    else let (first, second) = break (== '=') inst
-             (dst, other) =
-               if second == []
-                 then ([], first)
-                 else (first, second)
-             (cmp, jmp) = break (== ';') other
-         in C $
-            Operation
-              (parseDest dst)
-              (dropWhile (== '=') cmp)
-              (parseMaybeJump (dropWhile (== ';') jmp))
+  case x of
+    '@' -> mkAInstruction xs
+    --_   -> mkCInstruction inst
+
+mkCInstruction inst =
+  let (first, second) = break (== '=') inst
+      (dst, other) =
+        if second == []
+          then ([], first)
+          else (first, second)
+      (cmp, jmp) = break (== ';') other
+  in CInstruction $
+     Operation
+       (parseDest dst)
+       (dropWhile (== '=') cmp)
+       (parseJump (dropWhile (== ';') jmp))
+
+mkAInstruction :: String -> Either Error Instruction
+mkAInstruction i =
+  let mi = readMaybe i :: Maybe Int
+  in case mi of
+       Nothing -> Left $ i ++ " could not be parsed as an address."
+       Just i  -> Right (AInstruction i)
 
 parseDest :: String -> [Register]
-parseDest ""  = []
-parseDest dst = map parseReg dst
+parseDest = catMaybes . map parseReg
 
-parseReg :: Char -> Register
-parseReg 'D' = DReg
-parseReg 'A' = AReg
-parseReg 'M' = MReg
-parseReg _   = undefined
+parseReg :: Char -> Maybe Register
+parseReg c = readMaybe [c]
 
-parseMaybeJump :: String -> Maybe Jump
-parseMaybeJump "" = Nothing
-parseMaybeJump j  = Just (parseJump j)
-
-parseJump :: String -> Jump
-parseJump "JGT" = JGT
-parseJump "JEQ" = JEQ
-parseJump "JGE" = JGE
-parseJump "JLT" = JLT
-parseJump "JNE" = JNE
-parseJump "JLE" = JLE
-parseJump "JMP" = JMP
-parseJump _     = undefined
+parseJump :: String -> Maybe Jump
+parseJump = readMaybe
